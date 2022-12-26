5fe132152f5260f8b2733b7e43933b061c276b8a
KindaExists <ehrencastillo@gmail.com>
2022-12-26 18:30:18 +0800

    add images to themes & restructure themes

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,14 +1,17 @@
+import lightImage from '../assets/images/bg-light.jpg';
+import darkImage from '../assets/images/bg-dark.jpg';
+
 
 const Theme = {
-  colors: {
-    text: {
-      light: "#000",
-      dark: "#FFF",
-    },
-    bg: {
-      light: "#FFF",
-      dark: "#000",
-    },
+  light: {
+    text: '#000',
+    bg: '#FFF',
+    bgImage: lightImage,
+  },
+  dark: {
+    text: '#FFF',
+    bgColor: '#000',
+    bgImage: darkImage,
   },
 };
 
