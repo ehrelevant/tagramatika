549e196f90380474433bbe04076a12c2a498bae0
KindaExists <ehrencastillo@gmail.com>
2022-12-29 18:21:33 +0800

    changed textInput to be an editableContent div

--- src/components/TextInput.js
+++ src/components/TextInput.js
@@ -1,9 +1,10 @@
 import styled from 'styled-components';
 
-function TextInput({ onTextInputChange, textInputValue, textInputActive, fontSizeValue }) {
+function TextInput({ onTextInputChange, textInputActive, fontSizeValue }) {
   return (
     <TextInputWrapper textInputActive={textInputActive} fontSizeValue={fontSizeValue}>
-      <textarea className="text-input-area" placeholder="Magsulat dito..." onChange={onTextInputChange} value={textInputValue} />
+      {/* This is the only place where an HTML id is used, and this is very important */}
+      <div contentEditable id="text_input_area" className="text-input-area" onInput={onTextInputChange}></div>
     </TextInputWrapper>
   );
 }
@@ -23,13 +24,18 @@ const TextInputWrapper = styled.div`
   transition: 100ms ease-in-out;
 
   padding: 20px;
-
   margin: 10px 0 30px 0;
+
+  overflow-y: scroll;
 
   .text-input-area {
     height: 100%;
     width: 100%;
     box-sizing: border-box;
+
+    text-align: left;
+    overflow-wrap: break-word;
+    line-height: 1.2em;
 
     resize: none;
 
